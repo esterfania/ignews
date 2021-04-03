4676b36b636d0a7b6b117c4d5e24cfd62ef857b3
Ester Gama <esterfaniasantos@gmail.com>
2021-04-03 19:45:31 -0300

    add Auht0 atentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
+
 import styles from './style.module.scss';
+
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  const [session] = useSession();
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub className={styles.signInButtonLogged} />
-      esterfania
+      {session.user.name}
       <FiX className={styles.signInButtonCloseIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub className={styles.signInButtonNotLogged} />
       Sign in with GitHub
     </button>
