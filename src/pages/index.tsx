8306c906f4673cf6a34b9093685bfa2362536bfe
Ester Gama <esterfaniasantos@gmail.com>
2021-04-02 23:36:23 -0300

    install SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <h1 className={styles.title}>Hello</h1>;
 }
