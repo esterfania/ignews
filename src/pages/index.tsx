9c2e6333b6f2ff16cc7ad4468cb91f393fb89fe7
Ester Gama <esterfaniasantos@gmail.com>
2021-04-03 14:45:18 -0300

    change to static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IcC6EHKRm7dL2ydu9XciU3m');
 
   const product = {
@@ -50,5 +50,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
   return {
     props: { product },
+    revalidate: 60 * 60 * 48, //48 hours
   };
 };
