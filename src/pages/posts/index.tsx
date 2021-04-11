347931b5f55b8b8385b3f3b74cf705c93d6c47c6
Ester Gama <esterfaniasantos@gmail.com>
2021-04-11 00:20:59 -0300

    add posts on page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from './styles.module.scss';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -9,23 +23,49 @@ export default function Posts() {
       </Head>
       <main className={styles.PostContainer}>
         <div className={styles.PostItem}>
-          <a href='#'>
-            <time>12 de fevereiro de 2021</time>
-            <strong>Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how</p>
-          </a>
-          <a href='#'>
-            <time>12 de fevereiro de 2021</time>
-            <strong>Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how</p>
-          </a>
-          <a href='#'>
-            <time>12 de fevereiro de 2021</time>
-            <strong>Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'publication')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    }
+  );
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  console.log(posts);
+  return {
+    props: { posts },
+  };
+};
